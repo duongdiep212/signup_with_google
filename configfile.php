d94d6c273e4d29552e15765fd7961c46a3723cd6
shantanu21 <shantanu217@gmail.com>
2013-12-17 20:31:29 +0530

    ''

---
+++ configfile.php
@@ -0,0 +1,6 @@
+<?php
+$consumer_key='34760705102-aolcts0u7j60maegrc1uc5rqu7tlp149.apps.googleusercontent.com';
+$consumer_secret='akoSuqdjOZzGtbXraPmh8QGJ';
+$callback='http://localhost/signup_with_google/contact.php';
+$emails_count='25'; // max-results
+?>
