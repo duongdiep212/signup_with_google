b9015b363ca87881aaedb9f34f3aab22db7ef8b5
shantanu21 <shantanu217@gmail.com>
2013-12-17 20:46:46 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/","adminDIjKmD7","NkZvCtV-GiBk") or die (mysql_error());
+$con = mysqli_connect("localhost","root","") or die (mysql_error());
 
 mysqli_select_db($con,"signupgoogle"); 
 ?>
